e8587234bfac7ea110c6526935bbf9389f228d98
Amanda Zellmer <ajzellmer@gmail.com>
2016-09-08 14:56:14 -0700

    Add files via upload

--- PSHB_Burrow_Size.R
+++ PSHB_Burrow_Size.R
@@ -17,7 +17,33 @@ library(RCurl)
 urlPSHB <- getURL("https://raw.githubusercontent.com/OxyCompBio/OxyBiostats/master/PSHB_Burrow_Size.csv")
 PSHB <- read.csv(text = urlPSHB)
 
-#Plot Data
+#calculate means and variances
+meanPSHB <- ddply(PSHB, .(Species), colwise(mean))
+print(meanPSHB)
+sdPSHB <- ddply(PSHB, .(Species), colwise(sd))
+print(sdPSHB)
+
+#combine to create summary data file
+meanPSHB$sd <- sdPSHB$Length
+
+#test for significant differences using a t test
+PSHB.ttest <- t.test(Length ~ Species, data=PSHB)
+print(PSHB.ttest)
+
+####Plot Data
+
+#Plot stripchart with means and standard deviation
+ggplot(data=meanPSHB, aes(x=Species, y=Length))+
+  geom_point(size=5, color="grey50", shape=4)+
+  geom_errorbar(aes(ymin = Length-sd, ymax = Length+sd), color="grey60", size=1, width=0.1)+
+  geom_jitter(data=PSHB, aes(x=Species, y=Length, col=Species), size=2, alpha=1, width=0.25)+
+  theme_classic(base_size = 18)+
+  theme(axis.line.x = element_line(color="black", size = 2),
+        axis.line.y = element_line(color="black", size = 2),
+        legend.position="none")+
+  ylab("Length (cm)")
+
+#Plot boxplot
 ggplot()+
   geom_boxplot(data=PSHB, aes(x=Species, y=Length, col=Species))+
   theme_classic(base_size = 18)+
@@ -25,14 +51,4 @@ ggplot()+
         axis.line.y = element_line(color="black", size = 2),
         legend.position="none")+
   ylab("Length (cm)")
-
-#calculate means and variances
-meanPSHB <- ddply(PSHB, .(Species), colwise(mean))
-print(meanPSHB)
-sdPSHB <- ddply(PSHB, .(Species), colwise(sd))
-print(sdPSHB)
-
-#test for significant differences
-PSHB.ttest <- t.test(Length ~ Species, data=PSHB)
-print(PSHB.ttest)
 
